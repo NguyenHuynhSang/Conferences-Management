463393dbc845f309e2cb4be13b26afaf1866c42d
nguyenngocnhatminh <37469058+nguyenngocnhatminh@users.noreply.github.com>
2019-04-17 22:09:09 +0700

    Create, Index Speaker

---
+++ src/ConferencesManagement/ConferencesManagement/Areas/Admin/Controllers/SpeakerController.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using Models.Framework;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConferencesManagement.Areas.Admin.Controllers
+{
+    public class SpeakerController : Controller
+    {
+        // GET: Admin/Speaker
+        public ActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var dao = new SpeakerDao();
+            var result = dao.ListAllPaging(page, pageSize);
+            return View(result);
+        }
+
+        public ActionResult Create(Speaker speaker)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new SpeakerDao();
+                long id = dao.Insert(speaker);
+                if (id > 0)
+                {
+                    // chuyển hướng trang về admin/speaker/create
+                    RedirectToAction("Create", "Speker");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Them speaker loi");
+                }
+            }
+            return View("Create");
+
+        }
+
+    }
+}
---
+++ src/ConferencesManagement/Models/Models/SpeakerDao.cs
@@ -0,0 +1,42 @@
+﻿
+using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+
+
+namespace Models
+{
+    public class SpeakerDao
+    {
+            ConferencesManagementDbContext db = null;
+            public SpeakerDao()
+            {
+                db = new ConferencesManagementDbContext();
+            }
+
+            public long Insert(Speaker entity)
+            {
+                db.Speakers.Add(entity);
+                db.SaveChanges();
+                return entity.ID;
+            }
+
+            public Speaker GetByName(string Name)
+            {
+                return db.Speakers.SingleOrDefault(x => x.Name == Name);
+            }
+
+            public IEnumerable<Speaker> ListAllPaging(int page, int pageSize)
+            {
+                return db.Speakers.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+            }
+
+
+    }
+}
+
