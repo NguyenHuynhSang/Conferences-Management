66112cd42ed3a9bc5e0c2b7c0cdfdbb733c69b08
Bùi Đang Quy <37075303+mu4eve@users.noreply.github.com>
2019-04-19 15:26:08 +0700

    add FeedBackController, FeedBackDao, review Feedback's information, fix error to generate database without deleting code :v
    add FeedBackController, FeedBackDao, xem Feedback, sửa lỗi run database không cần delete

---
+++ src/ConferencesManagement/ConferencesManagement/Areas/Admin/Controllers/FeedBackController.cs
@@ -0,0 +1,23 @@
+﻿using Models.Framework;
+using Models;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConferencesManagement.Areas.Admin.Controllers
+{
+    public class FeedBackController : Controller
+    {
+        // GET: Admin/FeedBack
+        public ActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var dao = new FeedBackDao();
+            var result = dao.ListAllPaging(page, pageSize);
+            return View(result);
+        }
+
+    }
+}
---
+++ src/ConferencesManagement/Models/Models/FeedBackDao.cs
@@ -0,0 +1,29 @@
+﻿using Models.Framework;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class FeedBackDao
+    {
+        ConferencesManagementDbContext db = null;
+        public FeedBackDao()
+        {
+            db = new ConferencesManagementDbContext();
+        }
+
+        public FeedBack FindByID(int id)
+        {
+            return db.FeedBacks.SingleOrDefault(x => x.ID == id);
+
+        }
+        public IEnumerable<FeedBack> ListAllPaging(int page, int pageSize)
+        {
+            return db.FeedBacks.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+    }
+}
